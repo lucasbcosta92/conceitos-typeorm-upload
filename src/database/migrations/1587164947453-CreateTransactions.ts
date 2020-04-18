e430ae04fc5706c27a28b748d0152eb8ae42418e
Lucas Barbosa <39312124+lucasbcosta92@users.noreply.github.com>
2020-04-18 16:19:01 -0300

    Update Migration

--- src/database/migrations/1587164947453-CreateTransactions.ts
+++ src/database/migrations/1587164947453-CreateTransactions.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateTransactions1587164947453
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'transactions',
